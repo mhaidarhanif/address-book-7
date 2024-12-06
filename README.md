7ee0490e20a9a37d1e8998fd9b5ad625e8696d67
M Haidar Hanif <git@mhaidarhanif.com>
2024-12-06 19:52:44 +0700

    create initial contacts data

---
+++ README.md
@@ -0,0 +1,10 @@
+# Address Book
+
+A simple address book app built with React and Redux.
+
+## Features
+
+- Add contacts
+- Edit contacts
+- Delete contacts
+- Search contacts
---
+++ index.js
@@ -0,0 +1,21 @@
+const fruits = [
+  {
+    id: 1,
+    name: "Apple",
+    tags: ["red", "delicious"],
+    isFavorited: true,
+  },
+  {
+    id: 2,
+    name: "Orange",
+    tags: ["orange", "sour"],
+    isFavorited: false,
+  },
+  {
+    id: 3,
+    name: "Grape",
+    tags: ["purple", "green", "white"],
+  },
+];
+
+console.log(fruits);
